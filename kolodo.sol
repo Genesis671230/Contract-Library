365b92477844d16a605e85e744c58ae14482a38b
Genesis <97172297+Genesis671230@users.noreply.github.com>
2022-08-11 08:20:27 +0500

    Update kolodo.sol

--- kolodo.sol
+++ kolodo.sol
@@ -34,3 +34,78 @@ contract Kaleidoken is ERC721, Ownable {
     }
 
 }
+
+
+
+
+
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract Gig {
+
+
+    uint256 public _jobIds;
+
+    enum ProjectStatus{ACTIVE,PENDING,COMPLETE}
+
+    struct JobDetails{
+        string title;
+        uint256 deadline;
+        uint256 price;
+        bool applied;
+        address hiredFreelancer;
+        ProjectStatus status;
+    }
+
+  
+
+    mapping(uint256 => JobDetails) public JobsRegistry;
+    mapping(address => bool) private whiteListFreelancer;
+
+
+   
+    function whitelistF() external {
+        whiteListFreelancer[msg.sender] = true;
+    }
+
+
+    function PostJob(string memory _title,uint256 _deadline, uint256 _price) public payable {
+        require(msg.value >= _price,"Insufficient Balance");
+        _jobIds++;
+        uint256 time = block.timestamp + (_deadline + 1 days);
+        JobsRegistry[_jobIds] = JobDetails(_title,time,_price,false,address(0x0),ProjectStatus.ACTIVE);
+
+    }
+
+    function applyForJob(uint256 _jobid) external {
+        require( whiteListFreelancer[msg.sender] == true,"Only whitelisted freelancers can apply");
+        require(JobsRegistry[_jobid].status == ProjectStatus.ACTIVE,"The Job is not active");
+        JobsRegistry[_jobid].applied = true;        
+        JobsRegistry[_jobid].hiredFreelancer = msg.sender;
+        JobsRegistry[_jobid].status = ProjectStatus.PENDING;
+    }
+
+    function completeJob(uint256 _jobid) external {
+        require(JobsRegistry[_jobid].hiredFreelancer == msg.sender,"Only active freelancer can withdraw funds");
+        require(block.timestamp < JobsRegistry[_jobid].deadline,"Deadline is over");
+        require(JobsRegistry[_jobid].status == ProjectStatus.PENDING,"The Job is not active");
+        JobsRegistry[_jobid].status = ProjectStatus.COMPLETE;
+        payable(msg.sender).transfer(JobsRegistry[_jobid].price);
+
+    }
+
+    function listJobs() external view  returns(JobDetails[] memory ){
+        JobDetails[] memory list = new JobDetails[](_jobIds);
+        for(uint256 i = 1;i<=_jobIds;i++ ){
+            list[i - 1] = JobsRegistry[i];
+        }
+        return list;
+    }
+}
